f33b8b17474adc333d81393a7a565a439682b5fa
Lightning Auriga <lightning.auriga@gmail.com>
2021-10-22 15:18:51 -0400

    test: add placeholder tests for combine.trait.files

---
+++ tests/testthat/test_combine_trait_files.R
@@ -0,0 +1,19 @@
+test_that("combine.trait.files checks input types", {
+
+})
+
+test_that("combine.trait.files conducts a simple merge of phenotypes", {
+
+})
+
+test_that("combine.trait.files conducts a simple merge of covariates", {
+
+})
+
+test_that("combine.trait.files respects user suppression of batch variable", {
+
+})
+
+test_that("combine.trait.files finds and removes duplicates introduced by merging", {
+
+})
